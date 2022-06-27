ff8b601905cd7da0ba7c72159d52012159868a43
Josh Manders <josh@joshmanders.com>
2022-06-27 14:30:06 -0500

    add ability to extend/add things to context

--- lib/context.ts
+++ lib/context.ts
@@ -1,11 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { get, has, set } from 'lodash';
 
 export default class Context<Request = NextApiRequest, Response = NextApiResponse> {
   constructor(public req?: Request, public res?: Response) {}
 
+  set(path: string, value: any) {
+    return set(this, path, value);
+  }
+
+  get(path: string, defaultValue?: string) {
+    return get(this, path, defaultValue);
+  }
+
+  has(path: string) {
+    return has(this, path);
+  }
+
   attach(obj: Record<string, any>) {
     for (const [key, value] of Object.entries(obj)) {
-      this[key] = value;
+      this.set(key, value);
     }
 
     return this;
